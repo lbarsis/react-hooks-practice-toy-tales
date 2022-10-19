f7c5a2fbe31b85fc1d7b0fbacfc217d44546ed8a
Levi Barsis <lbarsis@gmail.com>
2022-10-18 19:14:31 -0700

    Done with assignment

--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ToyCard() {
+function ToyCard({ toy, onUpdateToy, onDeleteToy }) {
+  // const { name, image, likes, id } = toy;
+  // const [toyLikes, setToyLikes] = useState(toy.likes)
+
+  function like() {
+    
+    fetch(`http://localhost:3001/toys/${toy.id}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        likes: toy.likes + 1
+      })
+    })
+      .then(r => r.json())
+      .then(updatedToy => {
+        // setToyLikes(toyLikes => toyLikes + 1)
+        onUpdateToy(updatedToy)
+      })
+  }
+
+  function donate() {
+    fetch(`http://localhost:3001/toys/${toy.id}`, {
+      method: 'DELETE'
+    })
+      .then(r => r.json())
+      .then(() => {
+        onDeleteToy(toy)
+      })
+  }
+
   return (
     <div className="card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{toy.name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={toy.image}
+        alt={toy.name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <p>{toy.likes} Likes </p>
+      <button className="like-btn" onClick={like}>Like {"<3"}</button>
+      <button className="del-btn" onClick={donate}>Donate to GoodWill</button>
     </div>
   );
 }
--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,26 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({ toys, setToys }) {
+
+  function handleUpdatedToy(updatedToy) {
+    const updatedToys = toys.map(toy => {
+      return toy.id === updatedToy.id ? updatedToy : toy
+    })
+    setToys(updatedToys)
+  }
+
+  function handleDeletedToy(deletedToy){
+    const notDeletedToys = toys.filter(toy => toy.id !== deletedToy.id)
+    setToys(notDeletedToys)
+  }
+
+  const displayToys = toys.map(toy => {
+    return <ToyCard key={toy.id} toy={toy} onUpdateToy={handleUpdatedToy} onDeleteToy={handleDeletedToy}/>
+  })
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{displayToys}</div>
   );
 }
 
